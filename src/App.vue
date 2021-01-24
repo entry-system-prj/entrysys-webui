d3ce0afd6b3f9dcd17e7cd98c2f4f973ab65f110
mirko-san <41379069+mirko-san@users.noreply.github.com>
2021-01-24 20:56:14 +0900

    Add: Menu

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <v-menu offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-app-bar app color="primary" dark v-bind="attrs" v-on="on">
+          <v-app-bar-nav-icon />
+          <v-toolbar-title>Menu</v-toolbar-title>
+        </v-app-bar>
+      </template>
+      <v-list>
+        <v-list-item v-for="item in items" :key="item.to">
+          <v-list-item-title>
+            <router-link :to="item.to" v-text="item.text" />
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
 
     <v-main>
       <v-container>
@@ -45,5 +28,16 @@
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class App extends Vue {}
+export default class App extends Vue {
+  items = [
+    {
+      to: "/regist",
+      text: "登録フォーム"
+    },
+    {
+      to: "/list",
+      text: "登録者一覧"
+    }
+  ];
+}
 </script>
